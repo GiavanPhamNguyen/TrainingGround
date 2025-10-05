4701b7a38016ecacb9630d968483650395c52888
Giavan Nguyen <Giavanphamnguyen66@yahoo.com>
2025-10-05 00:19:58 -0500

    Made corrections to variables.

--- src/TrainingGround.kt
+++ src/TrainingGround.kt
@@ -7,7 +7,7 @@ fun main() {
 
     //prompt user to enter name and confirm name; output dummy's HP and user's HP; set the scene
     println("Welcome to the Training Ground, what is your name?")
-    var userName = readLine()
+    val userName = readln()
     println("Welcome to the Training Ground, $userName!")
     println("Across the arena, an intimidating dummy stares back at you")
     println("Its HP is $dummyHP")
@@ -15,7 +15,7 @@ fun main() {
 
     //give user option to continue or exit
     println("Do you want to fight the dummy? Type yes or no")
-    var userInput = readLine()
+    var userInput = readln()
     println()
 
     //if user decides to train, provide comprehensive rules and guidelines, then provide user with respective options
@@ -35,11 +35,11 @@ fun main() {
         println("Here are your choices of weapons: type 'fists' | 'staff' | 'sword' | 'bow'|")
         println("Weapon     Hit Chance      Damage Range  ")
         println("----------------------------------------")
-        println("Fists  |   72.7% (High)       | 1-6 (Low)")
+        println("Fists  |   91.7% (High)       | 1-6 (Low)")
         println("Staff  |   54.5% (Moderate)   | 2-14 (Moderate)")
         println("Sword  |   54.5% (Moderate)   | 4-15 (High)")
         println("Bow    |   36.4%% (Low)       | 7-15 (Deadly)")
-        var userWeapon = readLine()?.trim()?.lowercase()
+        val userWeapon = readln().trim().lowercase()
         println()
 
         //training depending on user's weapon choice, utilizing loops while user's HP and dummy's HP are above zero within 5 turns.
@@ -50,7 +50,7 @@ fun main() {
                 println("What do you want to do? type 'attack' | 'heal' | 'quit' | Turns Remaining: $turn")
                 println("$userName's HP: $userHP")
                 println("Dummy's HP: $dummyHP")
-                userInput = readLine()?.trim()?.lowercase()
+                userInput = readln().trim().lowercase()
                 println()
                 if (userInput == "attack") {
                     attackFists()
@@ -80,7 +80,7 @@ fun main() {
                 println("What do you want to do? type 'attack' | 'heal' | 'quit' | Turns remaining: $turn")
                 println("$userName's HP: $userHP")
                 println("Dummy's HP: $dummyHP")
-                userInput = readLine()?.trim()?.lowercase()
+                userInput = readln().trim().lowercase()
                 println()
                 if (userInput == "attack") {
                     attackStaff()
@@ -110,7 +110,7 @@ fun main() {
                 println("What do you want to do? type 'attack' | 'heal' | 'quit' | Turns remaining: $turn")
                 println("$userName's HP: $userHP")
                 println("Dummy's HP: $dummyHP")
-                userInput = readLine()?.trim()?.lowercase()
+                userInput = readln().trim().lowercase()
                 println()
                 if (userInput == "attack") {
                     attackSword()
@@ -140,7 +140,7 @@ fun main() {
                 println("What do you want to do? type 'attack' | 'heal' | 'quit' | Turns remaining: $turn")
                 println("$userName's HP: $userHP")
                 println("Dummy's HP: $dummyHP")
-                userInput = readLine()?.trim()?.lowercase()
+                userInput = readln().trim().lowercase()
                 println()
                 if (userInput == "attack") {
                     attackBow()
@@ -222,8 +222,8 @@ fun main() {
 
 //functions used for attacking with each respective weapons and healing
 fun attackFists(){//declare functions for attacking with fists
-    var hitChance = (2..12).random()
-    var damageChance = (1..6).random()
+    val hitChance = (2..12).random()
+    val damageChance = (1..6).random()
     if (hitChance <= 3){
         println("You rolled a $hitChance!")
         println("You missed.")
@@ -234,42 +234,42 @@ fun attackFists(){//declare functions for attacking with fists
         println("You rolled a(n) $hitChance!")
         println("You successfully hit the dummy.")
         if (damageChance == 6) {
-            var damage = 6
+            val damage = 6
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
             turn--
             println("YOU HAVE $turn TURNS REMAINING.")
         } else if (damageChance == 5) {
-            var damage = 5
+            val damage = 5
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
             turn--
             println("YOU HAVE $turn TURNS REMAINING.")
         } else if (damageChance == 4) {
-            var damage = 4
+            val damage = 4
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
             turn--
             println("YOU HAVE $turn TURNS REMAINING.")
         } else if (damageChance == 3) {
-            var damage = 3
+            val damage = 3
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
             turn--
             println("YOU HAVE $turn TURNS REMAINING.")
         } else if (damageChance == 2) {
-            var damage = 2
+            val damage = 2
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
             turn--
             println("YOU HAVE $turn TURNS REMAINING.")
         } else if (damageChance == 1) {
-            var damage = 1
+            val damage = 1
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
@@ -279,8 +279,8 @@ fun attackFists(){//declare functions for attacking with fists
     }
 }
 fun attackStaff(){//declare function for attacking with staff
-    var hitChance = (2..12).random()
-    var damageChance = (1..6).random()
+    val hitChance = (2..12).random()
+    val damageChance = (1..6).random()
     if (hitChance <= 5){
         println("You rolled a $hitChance!")
         println("You missed.")
@@ -291,42 +291,42 @@ fun attackStaff(){//declare function for attacking with staff
         println("You rolled a(n) $hitChance!")
         println("You successfully hit the dummy.")
         if (damageChance == 6) {
-            var damage = 10
+            val damage = 10
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
             turn--
             println("YOU HAVE $turn TURNS REMAINING.")
         } else if (damageChance == 5) {
-            var damage = 9
+            val damage = 9
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
             turn--
             println("YOU HAVE $turn TURNS REMAINING.")
         } else if (damageChance == 4) {
-            var damage = 8
+            val damage = 8
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
             turn--
             println("YOU HAVE $turn TURNS REMAINING.")
         } else if (damageChance == 3) {
-            var damage = (6..7).random()
+            val damage = (6..7).random()
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
             turn--
             println("YOU HAVE $turn TURNS REMAINING.")
         } else if (damageChance == 2) {
-            var damage = (4..5).random()
+            val damage = (4..5).random()
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
             turn--
             println("YOU HAVE $turn TURNS REMAINING.")
         } else if (damageChance == 1) {
-            var damage = (2..3).random()
+            val damage = (2..3).random()
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
@@ -336,8 +336,8 @@ fun attackStaff(){//declare function for attacking with staff
     }
 }
 fun attackSword(){//declare function for attacking with sword
-    var hitChance = (2..12).random()
-    var damageChance = (1..6).random()
+    val hitChance = (2..12).random()
+    val damageChance = (1..6).random()
     if (hitChance <= 5){
         println("You rolled a $hitChance!")
         println("You missed.")
@@ -348,42 +348,42 @@ fun attackSword(){//declare function for attacking with sword
         println("You rolled a(n) $hitChance!")
         println("You successfully hit the dummy.")
         if (damageChance == 6) {
-            var damage = 15
+            val damage = 15
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
             turn--
             println("YOU HAVE $turn TURNS REMAINING.")
         } else if (damageChance == 5) {
-            var damage = 14
+            val damage = 14
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
             turn--
             println("YOU HAVE $turn TURNS REMAINING.")
         } else if (damageChance == 4) {
-            var damage = (12..13).random()
+            val damage = (12..13).random()
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
             turn--
             println("YOU HAVE $turn TURNS REMAINING.")
         } else if (damageChance == 3) {
-            var damage = (10..11).random()
+            val damage = (10..11).random()
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
             turn--
             println("YOU HAVE $turn TURNS REMAINING.")
         } else if (damageChance == 2) {
-            var damage = (7..9).random()
+            val damage = (7..9).random()
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
             turn--
             println("YOU HAVE $turn TURNS REMAINING.")
         } else if (damageChance == 1) {
-            var damage = (4..6).random()
+            val damage = (4..6).random()
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
@@ -393,8 +393,8 @@ fun attackSword(){//declare function for attacking with sword
     }
 }
 fun attackBow(){//declare function for attacking with bow
-    var hitChance = (2..12).random()
-    var damageChance = (1..6).random()
+    val hitChance = (2..12).random()
+    val damageChance = (1..6).random()
     if (hitChance <= 7){
         println("You rolled an $hitChance!")
         println("You missed.")
@@ -405,42 +405,42 @@ fun attackBow(){//declare function for attacking with bow
         println("You rolled a(n) $hitChance!")
         println("You successfully hit the dummy.")
         if (damageChance == 6) {
-            var damage = 15
+            val damage = 15
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
             turn--
             println("YOU HAVE $turn TURNS REMAINING.")
         } else if (damageChance == 5) {
-            var damage = 14
+            val damage = 14
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
             turn--
             println("YOU HAVE $turn TURNS REMAINING.")
         } else if (damageChance == 4) {
-            var damage = 13
+            val damage = 13
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
             turn--
             println("YOU HAVE $turn TURNS REMAINING.")
         } else if (damageChance == 3) {
-            var damage = 12
+            val damage = 12
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
             turn--
             println("YOU HAVE $turn TURNS REMAINING.")
         } else if (damageChance == 2) {
-            var damage = 11
+            val damage = 11
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
             turn--
             println("YOU HAVE $turn TURNS REMAINING.")
         } else if (damageChance == 1) {
-            var damage = (8..10).random()
+            val damage = (8..10).random()
             println("You rolled a $damageChance!")
             println("You inflicted $damage points to the dummy's HP.")
             dummyHP -= damage
@@ -450,7 +450,7 @@ fun attackBow(){//declare function for attacking with bow
     }
 }
 fun heal() {//declare function for healing
-    var healChance = (1..6).random()
+    val healChance = (1..6).random()
     println("You rolled a $healChance! you healed yourself for $healChance points!")
     userHP += healChance
     turn--
@@ -458,8 +458,8 @@ fun heal() {//declare function for healing
     }
 
 fun dummyAttacks() {//declare function for dummy attacking back
-    var hitChance = (2..12).random()
-    var damage = (1..6).random()
+    val hitChance = (2..12).random()
+    val damage = (1..6).random()
     if (hitChance <= 7){
         println()
         println("The dummy attempted to strike, but missed.")
